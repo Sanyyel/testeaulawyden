2e42a94404d19c0b1b9810c495800bcaadad45ec
Sanyyel <danielsanches.minecraft@gmail.com>
2024-03-27 23:22:47 -0300

    alt finais

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = create_app()
 if __name__ == "__main__":
     
     app.run(debug=True)
+
+"""
+PROJECT Umbrella - Hotel Manager feito por:
+
+Ana Julia Ghirali da Silva 202302378481
+Daniel Gomes de Moraes Sanches 202102074721
+Leandro Rocha De Sena 202211214981
+Leonardo Manente Lourençon 202102074754
+Rodrigo Silva Pereira 202208717748
+"""
